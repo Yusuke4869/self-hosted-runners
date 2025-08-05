72fbad5f82d28a341500de6456b9cda8a113ddb4
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2025-08-05 12:48:50 +0900

    fix: add shebang line and fix to exit on errors

--- runner/build.sh
+++ runner/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 LATEST_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases/latest \
   | jq -r .tag_name \
   | sed 's/^v//'
--- runner/register_runner.sh
+++ runner/register_runner.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd /home/user/actions-runner
 
