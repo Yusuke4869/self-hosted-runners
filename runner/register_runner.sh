bd6da0ca00ae8f57a5f27cee2c17002e766da545
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2025-08-05 12:28:01 +0900

    feat: implement ephemeral actions runner

---
+++ runner/register_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/user/actions-runner
+
+REGISTRATION_TOKEN=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_PERSONAL_ACCESS_TOKEN}" \
+  https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token \
+  | jq -r .token)
+
+./config.sh \
+  --url https://github.com/${OWNER}/${REPO} \
+  --token ${REGISTRATION_TOKEN} \
+  --ephemeral \
+  --unattended
+
+./run.sh
