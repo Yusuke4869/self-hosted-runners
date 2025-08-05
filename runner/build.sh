6b4b382414446b2f2c689b91faa9ad2e4771856e
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2025-08-05 12:30:18 +0900

    feat: implement build script

---
+++ runner/build.sh
@@ -0,0 +1,13 @@
+LATEST_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases/latest \
+  | jq -r .tag_name \
+  | sed 's/^v//'
+)
+
+DOCKERFILE_DIR=$(dirname "$0")
+
+docker build \
+  --platform linux/x86_64 \
+  --build-arg RUNNER_VERSION=${LATEST_VERSION} \
+  -t actions-runner:latest \
+  -f "${DOCKERFILE_DIR}/Dockerfile" \
+  "${DOCKERFILE_DIR}"
