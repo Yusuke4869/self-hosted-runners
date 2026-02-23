1ecf44e8e7e5dd0bf237cf1690898965960e4003
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-02-23 21:48:27 +0900

    refactor(common): centralize environment loading in env module

--- src/common/middleware/github.ts
+++ src/common/middleware/github.ts
@@ -2,19 +2,14 @@ import { Webhooks } from "@octokit/webhooks";
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 
-const GITHUB_WEBHOOK_SECRET = Deno.env.get("GITHUB_WEBHOOK_SECRET");
-const webhooks = GITHUB_WEBHOOK_SECRET
-  ? new Webhooks({
-    secret: GITHUB_WEBHOOK_SECRET,
-  })
-  : null;
+import { env } from "../env.ts";
+
+const webhooks = new Webhooks({
+  secret: env.GITHUB_WEBHOOK_SECRET,
+});
 
 // ref: https://docs.github.com/ja/webhooks/using-webhooks/validating-webhook-deliveries#typescript-example
 export const githubMiddleware = createMiddleware(async (c, next) => {
-  if (!webhooks) {
-    throw new HTTPException(401, { message: "Unauthorized" });
-  }
-
   const signature = c.req.header("x-hub-signature-256");
   const body = await c.req.text();
 
